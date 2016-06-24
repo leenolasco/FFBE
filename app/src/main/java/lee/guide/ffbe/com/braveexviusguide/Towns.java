d474fe8762af97bb851a702df7110d9607c689c3
leenolasco <nolascolee@gmail.com>
2016-06-24 16:01:43 +0800

    Updated Sirens Info

--- app/src/main/java/lee/guide/ffbe/com/braveexviusguide/Towns.java
+++ app/src/main/java/lee/guide/ffbe/com/braveexviusguide/Towns.java
@@ -15,7 +15,7 @@ public class Towns extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.town_fragment, container, false);
+        View v = inflater.inflate(R.layout.siren_content_frag, container, false);
         return v;
     }
 }
